0f50a6df08664af6f8e8110183ff3b9aac577c2d
seilaregis <ninguemcriou13@gmail.com>
2024-07-17 01:57:16 -0300

    alteraçoes necessarias

---
+++ scripts/home.js
@@ -0,0 +1,43 @@
+// console.log("sexo")
+// const sexo = "sexooooooooooooooooooo"
+// var pinto = "seilateste"
+// console.log(sexo + pinto)
+
+// const numero = 5
+// const numer2 = 10
+// console.log(numero + numer2)
+
+// function soma(valor1, valor2) {
+//     const resultado = valor1 + valor2
+//     return resultado
+// }
+// console.log(soma(3,5))
+
+// const sexo = soma(3, 5)
+// console.log(sexo)
+
+// function mult(n1,n2,n3) {
+// const resultado = n1 * n2 * n3
+// return resultado
+// }
+// console.log(mult(2,2,2))
+
+const qualquercoisa = [12, "sexo", 56, "pinto"];
+
+// console.log(qualquercoisa[1])
+
+
+// for (let index = 0; index < qualquercoisa.length; index++) {
+//     const Element = qualquercoisa[index];
+//     console.log(Element)
+// }
+
+// let index = 0
+// while (index < qualquercoisa.length) {
+//     console.log(qualquercoisa[index])
+//     index++
+// }
+
+qualquercoisa.forEach(function (pintao){
+    console.log(pintao)
+})
